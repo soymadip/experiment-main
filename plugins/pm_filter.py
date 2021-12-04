71a3439ee8cec8980ef15c32e049da9a2892f8d6
soymadip <84225810+soymadip@users.noreply.github.com>
2021-12-04 18:50:06 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -59,7 +59,7 @@ async def filter(client, message):
                     [InlineKeyboardButton(text=f"{filename}",callback_data=f"subinps#{file_id}")]
                     )
         else:
-            await client.send_sticker(chat_id=message.from_user.id, sticker='CAADBQADIgMAAlrH6FWxbCzurm_WsAI')
+            await client.send_sticker(chat_id=message.from_user.id, sticker='CAACAgUAAxkBAAEDablhq2o0BdN75wya7UyosejlcGklvwAC7wMAAugIYVXBG5FSaN6FkCIE')
             return
 
         if not btn:
