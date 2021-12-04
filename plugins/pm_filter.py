736d6178257d6d2189e4026d4583beb3194f1ec0
soymadip <84225810+soymadip@users.noreply.github.com>
2021-12-04 17:54:28 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -346,4 +346,4 @@ async def cb_handler(client: Client, query: CallbackQuery):
         elif query.data == "pages":
             await query.answer()
     else:
-        await query.answer("കൌതുകും ലേശം കൂടുതൽ ആണല്ലേ👀",show_alert=True)
+        await query.answer("If you want to change page,then use abovr 'next page' button 👀",show_alert=True)
