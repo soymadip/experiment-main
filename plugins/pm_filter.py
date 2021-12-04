72e2bd1059ee3cad85af2928b5cd60dcd6f02eb5
soymadip <84225810+soymadip@users.noreply.github.com>
2021-12-04 18:19:12 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -120,7 +120,7 @@ async def group(client, message):
         if files:
             for file in files:
                 file_id = file.file_id
-                filename = f"{file.file_name}  [{get_size(file.file_size)} "
+                filename = f"{file.file_name}"
                 btn.append(
                     [InlineKeyboardButton(text=f"{filename}", url=f"https://telegram.dog/{nyva}?start=subinps_-_-_-_{file_id}")]
                 )
