e85b6a0007112b1167da1aa471811306365af9fe
soymadip <84225810+soymadip@users.noreply.github.com>
2022-01-08 09:26:19 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -81,7 +81,7 @@ async def filter(client, message):
             if API_KEY:
                 poster=await get_poster(search)
             if poster:
-                await message.reply_photo(photo=poster, caption=f"Here is What I Found In  Database named like <u>{search}<u>\n\n☢️<b>USE BUTTONS BETWEEN 7AM TO 11PM  ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_photo(photo=poster, caption=f"Here is What I Found In  Database named like <u>{search}</u>\n\n☢️<b>USE BUTTONS BETWEEN 7AM TO 11PM  ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
 
             else:
                 await message.reply_text(f"Here is What I Found In  Database named like <u>{search}<u>\n\n☢️<b>USE BUTTONS BETWEEN 7AM TO 11PM  ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
